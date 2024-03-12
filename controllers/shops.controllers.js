c83ced19c88a41c6e557ecda6f22a8c0c467e2a9
Johnmarin93 <Johnmarin93@users.noreply.github.com>
2024-03-12 00:56:02 -0500

    UpdateShops,interfaz,cards

--- controllers/shops.controllers.js
+++ controllers/shops.controllers.js
@@ -1,55 +1,76 @@
 import  Shops from '../models/Shops.model.js'
 
 export const getShops = async (req, res) => { 
-    const shops = await Shops.find()
-    res.json(shops)
+    try {
+        const shops = await Shops.find()
+            res.json(shops)
+    } catch (error) {
+        return res.status(404).json({message: " shops not found"})
+    }
 }
 
 export const createShops = async (req, res) => { 
-    const {CodUnico,Nombre,Direccion,Telefono,Contacto,Host,Mask,Gateway}=req.body
+    try {
+        const {CodUnico,Nombre,Direccion,Telefono,Contacto,Host,Mask,Gateway}=req.body
     
-    const newShops = new Shops({
-        CodUnico,
-        Nombre,
-        Direccion,
-        Telefono,
-        Contacto,
-        Host,
-        Mask,
-        Gateway,
-    })
-    const savedShops = await newShops.save()
-    res.json(savedShops);
+            const newShops = new Shops({
+                CodUnico,
+                Nombre,
+                Direccion,
+                Telefono,
+                Contacto,
+                Host,
+                Mask,
+                Gateway,
+            })
+            const savedShops = await newShops.save()
+            res.json(savedShops);
+    } catch (error) {
+        return res.status(404).json({message: " shops not found"})
+    }
 }
 
 export const getShop = async (req, res) => { 
-    const shops = await Shops.find({"CodUnico" : req.params.CodUnico})
+    try{
+        const shops = await Shops.find({"CodUnico" : req.params.CodUnico})
     if (shops.length === 0) return res.status(404).json({message: " shops not found"})
         res.json(shops)
+    }catch (error){
+        return res.status(404).json({message: " shops not found"})
+    }
+    
 }
 
 export const deleteShops = async (req, res) => { 
-    const shops = await Shops.deleteOne({"CodUnico" : req.params.CodUnico})
-    if (shops.deletedCount !== 1)return res.status(404).json({message: " shops not found"})
-    return res.sendStatus(204);
+    try {
+        const shops = await Shops.deleteOne({"CodUnico" : req.params.CodUnico})
+         if (shops.deletedCount !== 1)return res.status(404).json({message: " shops not found"})
+            return res.sendStatus(204);
+    } catch (error) {
+        return res.status(404).json({message: " shops not found"})
+    }
     
 }
 
 export const updateShops = async (req, res) => { 
-    const body = req.body;
-    const shops = await Shops.findOneAndUpdate(
-        {
-            "CodUnico" : req.params.CodUnico
-        },
-        {
-            ...body
-        },
-        {
-            new:true,
-        }
-        
-    );
-    if (!shops) return res.status(404).json({message: " shops not found"})
-    res.json({
-        data:shops})
+    try {
+            const body = req.body;
+            const shops = await Shops.findOneAndUpdate(
+                {
+                    "CodUnico" : req.params.CodUnico
+                },
+                {
+                    ...body
+                },
+                {
+                    new:true,
+                }
+                
+                );
+            if (!shops) return res.status(404).json({message: " shops not found"})
+            res.json({
+                data:shops})
+    } catch (error) {
+        return res.status(404).json({message: " shops not found"})
+    }
 } 
